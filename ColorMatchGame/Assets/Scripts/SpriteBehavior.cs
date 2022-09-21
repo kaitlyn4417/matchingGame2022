4d5f181cbae2339556f0895d48caecf9a510b5a9
kaitlyng <10957070@uvu.edu>
2022-09-21 16:54:35 -0600

    finished matching game part 2 lab

--- ColorMatchGame/Assets/Scripts/SpriteBehavior.cs
+++ ColorMatchGame/Assets/Scripts/SpriteBehavior.cs
@@ -14,4 +14,9 @@ public class SpriteBehavior : MonoBehaviour
     {
         rendererObj.color = obj.value;
     }
+
+	public void ChangeRendererColor(ColorIDDataList obj)
+	{
+		rendererObj.color = obj.currentColor.value;
+	}
 }
